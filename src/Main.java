607b7fe5a38335358f558157453019c5a4481c5b
AlexSMGr54 <alex.smg.r54@gmail.com>
2020-05-29 14:17:30 +0700

    Initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 18360_50;  //Цена билета в копейках
+        int mileCost = 20_00;       //Стоимость мили в копейках
+
+        System.out.println(ticketCost / mileCost);
+    }
+}
